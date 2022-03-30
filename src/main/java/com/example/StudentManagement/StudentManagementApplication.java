33d12ab6f896b830107113042246b82c4496e98f
AdijosAmigos <89484693+AdijosAmigos@users.noreply.github.com>
2022-03-30 14:01:16 +0200

    dodanie prostego security

--- src/main/java/com/example/StudentManagement/StudentManagementApplication.java
+++ src/main/java/com/example/StudentManagement/StudentManagementApplication.java
@@ -1,5 +1,6 @@
 package com.example.StudentManagement;
 
+import com.example.StudentManagement.security.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -12,6 +13,14 @@ public class StudentManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean (){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new JwtFilter());
+        filterRegistrationBean.setUrlPatterns(Collections.singleton("/students*"));
+        return filterRegistrationBean;
     }
 
 
---
+++ src/main/java/com/example/StudentManagement/security/LoginApi.java
@@ -0,0 +1,29 @@
+package com.example.StudentManagement.security;
+
+import com.example.StudentManagement.model.Student;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class LoginApi {
+
+    @PostMapping("/login")
+    public String login(@RequestBody Student student){
+
+        long currentTimeMillis = System.currentTimeMillis();
+        return Jwts.builder()
+                .setSubject(student.getFirstName())
+                .claim("roles", "student")
+                .setIssuedAt(new Date(currentTimeMillis))
+                .setExpiration(new Date(currentTimeMillis + 1000 * 60))
+                .signWith(SignatureAlgorithm.HS512, student.getLastName())
+                .compact();
+
+    }
+}
